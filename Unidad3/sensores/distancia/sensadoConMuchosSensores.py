b08918f053abf7d5e3445d5ad690f9bc24613632
juani <juani.trotti@gmail.com>
2021-12-15 17:55:48 -0300

    ejercicio sensado con muchos sensores

--- Unidad3/sensores/distancia/sensadoConMuchosSensores.py
+++ Unidad3/sensores/distancia/sensadoConMuchosSensores.py
@@ -7,17 +7,19 @@ max_velocity = 6.28
 
 robot = Robot()
 
-# podemos crear y activar uno por uno pero si los sensores tienen los nombres de la siguiente manera "nombredelsensor NRO" podemos
-# crear un array que contenga todos los sensores
-
+# podemos crear y activar uno por uno pero si los sensores tienen los nombres de la siguiente manera "nombredelsensor NRO"
+# creamos un array que contenga todos los sensores
 sensoresDistancia = []
 
+# usamos un for para agregar todos los sensores en las posiciones del array y los activamos. 
+# IMPORTANTE, LOS ARRAYS EN PYTHON COMIENZAN EN LA POSICIÓN 0, es decir que los sensores que tengas tienen que ser así: sensor0, sensor1 .... sensor N
 for i in range(4):
-    sensoresDistancia.append(robot.getDevice("distance sensor" + str(i)))
-    sensoresDistancia[i].enable(timeStep)
+    sensoresDistancia.append(robot.getDevice("distance sensor" + str(i))) # con la funcion str() convertimos i (que es un entero) en un string y asi podemos concatenar. 
+    sensoresDistancia[i].enable(timeStep) # aca el elemento que está en la posición i del array lo activamos.
 
 while robot.step(timeStep) != -1:
-    
+
+    # usamos un for para consultar el valor de cada sensor
     for i in range(4):
-        distancia = sensoresDistancia[i].getValue()
-        print("distancia" + str(i) + ":" + str(distancia))
+        distancia = sensoresDistancia[i].getValue() # obtenemos el valor del sensor
+        print("distancia" + str(i) + ":" + str(distancia)) # imprimimos el sensor con su valor
