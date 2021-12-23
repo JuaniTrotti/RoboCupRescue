297fa62ca00b4d17eed20b2609d60fc5055b5a0e
RichoM <richi.moran@gmail.com>
2021-12-23 18:54:27 -0300

    Agrego algunos ejemplos sencillos del uso de los sensores de distancia

---
+++ Unidad3/sensores/distancia/ejemplo_4.py
@@ -0,0 +1,17 @@
+# Ejemplo: Mostrando el valor del sensor ps0
+from controller import Robot
+
+TIME_STEP = 32
+
+robot = Robot()
+
+distSensors = []
+for i in range(8):
+    sensor = robot.getDevice("ps" + str(i))
+    sensor.enable(TIME_STEP)
+    distSensors.append(sensor)
+
+while robot.step(TIME_STEP) != -1:
+    for i in range(8):
+        print(f"Distancia sensor {i}: {distSensors[i].getValue()}")
+    print("================")
