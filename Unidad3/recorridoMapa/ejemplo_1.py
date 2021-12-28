df93317107d1fb3098423c00604fe529952e52e8
RichoM <richi.moran@gmail.com>
2021-12-28 08:14:03 -0300

    Agrego comentarios

--- Unidad3/recorridoMapa/ejemplo_1.py
+++ Unidad3/recorridoMapa/ejemplo_1.py
@@ -40,9 +40,14 @@ def turnLeft():
     delay(1)
 
 while robot.step(TIME_STEP) != -1:
+    # Leo los sensores de distancia y guardo los valores en variables
     dist_left = ps5.getValue()
     dist_front = ps7.getValue()
 
+    # Ajustamos la velocidad de las ruedas dependiendo de la distancia
+    # con la pared izquierda. Si nos acercamos mucho giramos ligeramente
+    # a la derecha. Si nos alejamos giramos ligeramente a la izquierda.
+    # El objetivo es mantenernos en un rango entre 0.035 y 0.045
     if dist_left < 0.035:
         wheelL.setVelocity(1.0 * MAX_VEL)
         wheelR.setVelocity(.95 * MAX_VEL)
@@ -53,6 +58,10 @@ while robot.step(TIME_STEP) != -1:
         wheelL.setVelocity(1.0 * MAX_VEL)
         wheelR.setVelocity(1.0 * MAX_VEL)
 
+    # Si la distancia a la pared izquierda supera los 0.1 metros
+    # significa que tenemos que girar 90 grados hacia la izquierda.
+    # Si en cambio nos encontramos con una pared adelante, tenemos
+    # que girar 90 grados a la derecha.
     if dist_left > 0.1:
         turnLeft()
     elif dist_front < 0.05:
