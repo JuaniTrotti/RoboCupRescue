223203510f3fc74883e321d56f8937f2518f7987
RichoM <richi.moran@gmail.com>
2022-01-07 09:26:32 -0300

    Agrego ejemplos de uso del sensor de color

---
+++ Unidad4/colorPiso/ejemplo_2.py
@@ -0,0 +1,26 @@
+# Ejemplo: Imprimir el color RGB que ve el sensor de color
+from controller import Robot
+
+TIME_STEP = 32
+MAX_VEL = 6.28
+
+robot = Robot()
+
+# Obtener el sensor de color y habilitarlo
+colorSensor = robot.getDevice("colour_sensor")
+colorSensor.enable(TIME_STEP)
+
+def esPantano(r, g, b):
+    # El color del pantano es (R:244, G:221, B:141), así que analizamos cada 
+    # canal por separado y usamos un umbral para comparar.
+    return abs(r - 244) < 10 \
+        and abs(g - 221) < 10 \
+        and abs(b - 141) < 10
+
+while robot.step(TIME_STEP) != -1:
+    # Acceder al color detectado por el sensor. El canal A lo ignoramos.
+    b, g, r, a = colorSensor.getImage()
+
+    # Si llegamos a un pantano, mostramos un mensaje
+    if esPantano(r, g, b):
+        print(f"{robot.getTime():.2f}: Ojo! Pantano!")
