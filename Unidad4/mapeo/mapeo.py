99f88d743a59d5a03ed0b1a6f6e41a7201eade79
RichoM <richi.moran@gmail.com>
2022-01-13 19:47:02 -0300

    Agrego trackeo de los tiles visitados y sus conexiones

--- Unidad4/mapeo/mapeo.py
+++ Unidad4/mapeo/mapeo.py
@@ -1,9 +1,18 @@
 from controller import Robot
+from enum import Enum
 import math
 from random import random
 
 TIME_STEP = 32
 MAX_VEL = 6.28
+
+FOLDER = r"X:\RoboCupRescue\temp"
+
+class Direction(Enum):
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
 
 robot = Robot() 
 
@@ -35,7 +44,7 @@ gps = robot.getDevice("gps")
 gps.enable(TIME_STEP)
 
 position = None
-initialPosition = None
+direction = None
 rotation = 0.25*math.tau
 beginTime = robot.getTime()
 currentTime = beginTime
@@ -44,21 +53,51 @@ deltaTime = 0
 x = 0
 y = 0
 
+grid = {}
+
+def tile(x, y):
+    if (x, y) in grid: return grid[(x, y)]
+    t = {"x": x, "y": y,
+         "up": None,
+         "down": None,
+         "left": None,
+         "right": None}
+    grid[(x, y)] = t
+    return t
+
+def connectTiles(prev, cur, direction):
+    if direction == Direction.UP:
+        prev["up"] = cur
+        cur["down"] = prev
+    elif direction == Direction.RIGHT: 
+        prev["right"] = cur
+        cur["left"] = prev
+    elif direction == Direction.DOWN:
+        prev["down"] = cur
+        cur["up"] = prev
+    elif direction == Direction.LEFT:
+        prev["left"] = cur
+        cur["right"] = prev
+
 def updatePosition():
     global position, initialPosition
     x, _, y = gps.getValues()
     position = {"x": x, "y": y}
-    if initialPosition == None: initialPosition = position
 
 def updateRotation():
-    global currentTime, deltaTime, rotation
+    global currentTime, deltaTime, rotation, direction
     lastTime = currentTime
     currentTime = robot.getTime()
     deltaTime = currentTime - lastTime
     
     vel, _, _ = gyro.getValues()
     rotation += (vel * deltaTime)
-    rotation %= math.tau # Normalizamos el valor del ángulo
+    rotation %= math.tau
+    
+    if looking_up(rotation): direction = Direction.UP
+    elif looking_right(rotation): direction = Direction.RIGHT
+    elif looking_down(rotation): direction = Direction.DOWN
+    elif looking_left(rotation): direction = Direction.LEFT
     
 def updateVars():
     updatePosition()
@@ -139,6 +178,39 @@ def avanzar(distance):
     wheelL.setVelocity(0)
     wheelR.setVelocity(0)
 
+def writeGrid():
+    min_x = 0
+    min_y = 0
+    max_x = 0
+    max_y = 0
+    for x, y in grid:
+        if x < min_x: min_x = x
+        if y < min_y: min_y = y
+        if x > max_x: max_x = x
+        if y > max_y: max_y = y
+
+    rows = []
+    for y in range(min_y, max_y + 1):
+        row = []
+        for x in range(min_x, max_x + 1):
+            t = grid.get((x, y))
+            chars = [["+", "-", "+"],["|", "?", "|"],["+", "-", "+"]]
+            if t != None:
+                chars[1][1] = "@" if x == 0 and y == 0 else " "
+                if t["up"] != None: chars[0][1] = " "
+                if t["right"] != None: chars[1][2] = " "
+                if t["down"] != None: chars[2][1] = " "
+                if t["left"] != None: chars[1][0] = " "
+            row.append(chars)
+        rows.append(row)
+    
+    with open(FOLDER + "/map.txt", "w") as f:
+        for row in rows:
+            for i in range(0, 3):
+                for chars in row:
+                    f.write("".join(chars[i]))
+                f.write("\n")
+
 while step() != -1:
 
     if ps7.getValue() < 0.08:        
@@ -147,24 +219,24 @@ while step() != -1:
         else:
             girar(-0.25*math.tau) # Girar izquierda
     else:
+        prev = tile(x, y)
         avanzar(0.12) # Avanzar 1 baldosa
             
-        if looking_up(rotation): y -= 1
-        if looking_right(rotation): x += 1
-        if looking_down(rotation): y += 1
-        if looking_left(rotation): x -= 1
-
+        if direction == Direction.UP: y -= 1
+        elif direction == Direction.RIGHT: x += 1
+        elif direction == Direction.DOWN: y += 1
+        elif direction == Direction.LEFT: x -= 1
+        
+        cur = tile(x, y)
+        connectTiles(prev, cur, direction)
+        writeGrid()
 
     l = ps5.getValue() < 0.1
     r = ps2.getValue() < 0.1
     f = ps7.getValue() < 0.1
     b = ps4.getValue() < 0.1
     print(f"F: {f}, R: {r}, B: {b}, L: {l}")
-
-    if looking_up(rotation): print("UP")
-    if looking_right(rotation): print("RIGHT")
-    if looking_down(rotation): print("DOWN")
-    if looking_left(rotation): print("LEFT")
+    print(direction)
 
     print([x, y])
 
